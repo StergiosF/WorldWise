95e2aba8a9ed681ae959c51e391a55aab0adecbe
Stergios Fotoglou <stfotoglou@gmail.com>
2025-02-10 02:23:30 +0200

    BASE_URL change

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "https://worldwise-j80v.onrender.com/cities";
+const BASE_URL = "https://worldwise-j80v.onrender.com";
 
 const CitiesContext = createContext();
 
